d52c7051fcf4b8d5b5edb25310b1f27b18214982
prakashkumar <prakashyadavpatna2007@gmail.com>
2026-09-11 22:39:32 +0530

    update

---
+++ src/variable/implicity.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class implicity {
+    public static void main(String[] args){
+        int a = 20;
+
+        double b = a;
+
+        System.out.println(b);
+    }
+}
---
+++ src/variable/use.java
@@ -0,0 +1,18 @@
+package variable;
+//Create variables for an integer, decimal number, character and boolean value and print them
+public class use {
+    public static void main (String[] args) {
+
+        int age = 20;
+        double num = 2000.3;
+        char grade = 'A';
+        boolean id = true;
+
+
+        System.out.println("Age = " + age);
+        System.out.println("Salry = " + num);
+        System.out.println("Grade = " + grade);
+        System.out.println("Id = " + id);
+
+    }
+}
